f79cfe7bd514e2236d4cc7bfd12ca1040668cf59
root <marsilas81@gmail.com>
2024-03-23 13:16:12 -0700

    Palindromes

---
+++ 0x17-doubly_linked_lists/palindromes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+largest_palindrome = 0
+
+# Start from the largest possible product and work downwards
+for i in range(999, 99, -1):
+    # Start from i to avoid redundant checks
+    for j in range(i, 99, -1):
+        product = i * j
+        if product <= largest_palindrome:
+            break  # No need to continue inner loop if product is smaller
+        if is_palindrome(product):
+            largest_palindrome = product
+
+print(largest_palindrome)
