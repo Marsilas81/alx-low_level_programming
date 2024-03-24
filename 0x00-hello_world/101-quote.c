2af8f3c0c6753eb8e51a89d777db610d6d7aec98
root <marsilas81@gmail.com>
2024-03-24 04:59:10 -0700

    a C program that prints string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,24 @@
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specified string to standard output
+ *              using the write system call
+ *
+ * Return: Always 1 (Exit failure)
+ */
 int main(void)
 {
     char name[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = strlen(name);
 
-    write(1, name, strlen(name));
-    exit(1);
+    if (write(STDOUT_FILENO, name, len) != len)
+    {
+        return (EXIT_FAILURE);
+    }
+
+    return (EXIT_SUCCESS);
 }
